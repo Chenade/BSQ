d27207276de48f364d21665f7709b718cca81861
Chenade <chenade0312@gmail.com>
2022-03-28 21:58:54 +0200

    utils.c

---
+++ utils.c
@@ -0,0 +1,25 @@
+#include "rush.c"
+
+int	ft_atoi(char *str)
+{
+	int	ans;
+	int	is_neg;
+	int	i;
+
+	ans = 0;
+	is_neg = 1;
+	i = 0;
+	while (str[i] == 32 || (str[i] > 6 && str[i] < 14))
+		i += 1;
+	while (str[i] == 43 || str[i] == 45)
+	{
+		is_neg *= 44 - str[i];
+		i += 1;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		ans = ans * 10 + (str[i] - 48);
+		i += 1;
+	}
+	return (ans * is_neg);
+}
